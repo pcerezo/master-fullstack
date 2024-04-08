533cb2a437dc5752a460e8c037166034cd97f969
pcerezo <pcerezo70@gmail.com>
2024-04-08 23:36:00 +0200

    Mejorada presentación de portada de la página. Añadidos botones de acción para cada post.

--- blog-angular/src/app/components/home/home.component.ts
+++ blog-angular/src/app/components/home/home.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { PostService } from '../../services/post.service';
 import { global } from '../../services/global';
 import { Post } from '../../models/post';
+import { RouterLink } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   providers: [PostService]
@@ -15,13 +18,18 @@ export class HomeComponent {
   public page_title: string;
   public url: string;
   public listaPosts: any = [];
+  public identity;
 
   constructor(
-    private _postService: PostService
+    private _postService: PostService,
+    private _userService: UserService
   ) {
     this.page_title = 'Inicio';
     this.url = global.url;
+    this.identity = this._userService.getIdentity();
+  }
 
+  ngOnInit() {
     this.listarPosts();
   }
 
