c14637cf6d103e8dc9d0529bff065c4d4b55efea
pcerezo <pcerezo70@gmail.com>
2024-04-16 23:50:26 +0200

    Añadida obtención de artículos por categoría con sus opciones según seas propietario o no.

--- blog-angular/src/app/components/category-detail/category-detail.component.ts
+++ blog-angular/src/app/components/category-detail/category-detail.component.ts
@@ -1,31 +1,78 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink, RouterModule } from '@angular/router';
 import { CategoryService } from '../../services/category.service';
 import { global } from '../../services/global';
+import { UserService } from '../../services/user.service';
+import { PostService } from '../../services/post.service';
 
 @Component({
   selector: 'app-category-detail',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './category-detail.component.html',
-  styleUrl: './category-detail.component.css'
+  styleUrl: './category-detail.component.css',
+  providers: [CategoryService, UserService, PostService]
 })
 export class CategoryDetailComponent {
   public page_title: string;
   public url;
+  public listaPosts: any = [];
+  public identity;
+  public token;
 
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
-    private _categoryService: CategoryService
+    private _categoryService: CategoryService,
+    private _userService: UserService,
+    private _postService: PostService
   ) {
-    this.page_title = "Artículos de la categoría ";
+    this.page_title = "";
     this.url = global.url;
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getCategory();
+    this.getPostsByCategory();
+  }
+
+  getCategory() {
+    this._route.params.subscribe(params => {
+      let id = + params['id'];
+
+      this._categoryService.getCategory(id).subscribe( response => {
+        if (response.status == "success") {
+          this.page_title = response.category.name;
+        }
+      });
+    });
+  }
 
   getPostsByCategory() {
-    
+    this._route.params.subscribe(params => {
+      let id = + params['id'];
+
+      this._categoryService.getPosts(id).subscribe(
+        response => {
+          if (response && response.status == "success") {
+            this.listaPosts = response.message;
+          }
+          else {
+            this._router.navigate(['/inicio']);
+          }
+        }
+      );
+    });
   }
+
+  deletePost(id: number) {
+    this._postService.delete(this.token!, id).subscribe(
+      response => {
+        this.getPostsByCategory();
+      }
+    );
+  }
+
 }
